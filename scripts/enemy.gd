3b8e0b35e9c72460733453551340a8be25f9b35f
Ssanntii <santiagord32@gmail.com>
2025-09-12 19:00:49 -0300

    RPG Tale

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -7,9 +7,27 @@ const FRICTION = 300.0
 var player: CharacterBody2D = null
 var player_chase := false
 
-@onready var animated_sprite = $AnimatedSprite2D
+var health = 100.0
+var player_inattack_zone= false
+var can_take_damage = true
+
+@onready var anim = $AnimatedSprite2D
+
+func _ready() -> void:
+	var rand_anim = randi_range(1,3)
+	if rand_anim == 1:
+		anim.play("front_idle")
+	else:
+		if rand_anim == 2:
+			anim.play("side_idle")
+			anim.flip_h = false
+		else:
+			anim.play("side_idle")
+			anim.flip_h = true
 
 func _physics_process(delta: float) -> void:
+	deal_with_damage()
+	
 	if player_chase and player != null:
 		var direction = (player.position - position).normalized()
 		velocity += direction * ACCEL * delta
@@ -37,21 +55,21 @@ func update_animation(current_velocity: Vector2):
 
 	# Comparamos si el movimiento es más horizontal o vertical.
 	if abs(current_velocity.x) > abs(current_velocity.y):
-		animated_sprite.play("side_walk")
+		anim.play("side_walk")
 		# La dirección del sprite (izquierda/derecha) depende del signo de la velocidad en x.
-		animated_sprite.flip_h = current_velocity.x < 0
+		anim.flip_h = current_velocity.x < 0
 	else:
 		if current_velocity.y > 0:
-			animated_sprite.play("front_walk")
+			anim.play("front_walk")
 		else:
-			animated_sprite.play("back_walk")
+			anim.play("back_walk")
 
 func play_idle_animation():
-	var current_animation = animated_sprite.animation
+	var current_animation = anim.animation
 	
 	if "walk" in current_animation:
 		var idle_animation = current_animation.replace("walk", "idle")
-		animated_sprite.play(idle_animation)
+		anim.play(idle_animation)
 
 # --- Las funciones de detección no cambian ---
 func _on_detection_area_body_entered(body: Node2D) -> void:
@@ -64,3 +82,28 @@ func _on_detection_area_body_exited(body: Node2D) -> void:
 		# Ya no ponemos player en null inmediatamente, para que el enemigo
 		# sepa dónde estaba por última vez, pero dejamos de perseguirlo.
 		player_chase = false
+
+func enemy():
+	pass
+
+func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = true
+
+func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = false
+
+func deal_with_damage():
+	if player_inattack_zone and Global.player_current_attack:
+		if can_take_damage:
+			health -= 20
+			$take_damage_cooldown.start()
+			can_take_damage = false
+			print("Slime health: ", health)
+			if health <= 0:
+				self.queue_free()
+
+
+func _on_take_damage_cooldown_timeout() -> void:
+	can_take_damage = true
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,12 @@
 extends CharacterBody2D
 
+var enemy_inattack_range = false
+var enemy_attack_cooldown = true
+var health = 100.0
+var player_alive = true
+
+var attack_ip = false
+
 const MAX_SPEED = 100
 const accel = 1500
 const friction = 600
@@ -12,6 +19,14 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	player_movement(delta)
+	enemy_attack()
+	attack()
+	
+	if health <= 0:
+		player_alive = false
+		health = 0
+		print("Player has been killed")
+		self.queue_free()
 	
 func get_input():
 	input.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
@@ -58,21 +73,74 @@ func play_anim(movement):
 		if movement == 1:
 			anim.play("side_walk")
 		elif movement == 0:
-			anim.play("side_idle")
+			if attack_ip == false:
+				anim.play("side_idle")
+				
 	if dir == "left":
 		anim.flip_h = true
 		if movement == 1:
 			anim.play("side_walk")
 		elif movement == 0:
-			anim.play("side_idle")
+			if attack_ip == false:
+				anim.play("side_idle")
+				
 	if dir == "down":
 		if movement == 1:
 			anim.play("front_walk")
 		elif movement == 0:
-			anim.play("front_idle")
+			if attack_ip == false:
+				anim.play("front_idle")
+				
 	if dir == "up":
 		if movement == 1:
 			anim.play("back_walk")
 		elif movement == 0:
-			anim.play("back_idle")
-			
+			if attack_ip == false:
+				anim.play("back_idle")
+
+func player():
+	pass
+
+func _on_player_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_inattack_range = true
+func _on_player_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_inattack_range = false
+
+func enemy_attack():
+	if enemy_inattack_range and enemy_attack_cooldown:
+		health -= 20
+		enemy_attack_cooldown = false
+		$attack_cooldown.start()
+		print(health)
+
+func _on_attack_cooldown_timeout() -> void:
+	enemy_attack_cooldown = true
+
+func attack():
+	var dir = current_dir
+	
+	if Input.is_action_just_pressed("attack"):
+		Global.player_current_attack = true
+		attack_ip = true
+		if dir == "right":
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.play("side_attack")
+			$deal_attack_timer.start()
+		if dir == "left":
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("side_attack")
+			$deal_attack_timer.start()
+		if dir == "down":
+			$AnimatedSprite2D.play("front_attack")
+			$deal_attack_timer.start()
+		if dir == "up":
+			$AnimatedSprite2D.play("back_attack")
+			$deal_attack_timer.start()
+
+
+func _on_deal_attack_timer_timeout() -> void:
+	$deal_attack_timer.stop()
+	Global.player_current_attack = false
+	attack_ip = false
